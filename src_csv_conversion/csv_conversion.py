8f76f60f81f518d8b8230d064c10f203af7dbd22
MrErikasMr <kacerikas@gmail.com>
2023-07-26 11:15:24 +0100

    done csv_conversion, need testing

--- src_csv_conversion/csv_conversion.py
+++ src_csv_conversion/csv_conversion.py
@@ -1,32 +1,41 @@
 import psycopg2
 import csv
 import re
+import os
 
-secrets = [] 
-file_path = '../de-totes-bags/secrets.txt'
-try:
-    with open(file_path, 'r') as file:
-        # Step 2: Read the file using read() method
-        # file_contents = file.read()
-        # You can also use readline() to read a single line at a time
-        for i in range(4):
-            line = file.readline()
-            secrets.append(line)
-        print(secrets[0])
+secrets = []
+file_path = 'secrets.txt'
+regex_host = ":"
+regex_host_end = ","
+input_labels = ["User", "Host", "Port", "Database", "Password"]
+inputs = {}
+
+with open(file_path, 'r') as file:
+    for i in range(5):
+        line = file.readline()
+        secrets.append(line)
+
+for label, secret in zip(input_labels, secrets):
+    input_value = ""
+    start_inputting = False
+    for char in secret:
+        if start_inputting:
+            input_value += char
+        if char == regex_host:
+            start_inputting = True
+        if char == regex_host_end:
+            input_value = input_value[:-1]
+            start_inputting = False
+            break
+    inputs[label] = input_value
 
 
-except FileNotFoundError:
-    print(f"File not found: '{file_path}'")
 
-# The file will be automatically closed after the 'with' block ends.
-
-
-
-host = "nc-data-eng-totesys-production.chpsczt8h1nu.eu-west-2.rds.amazonaws.com"
-port = 5432
-database = "totesys"
-user = "project_user_1"
-password = "wgvk8E1JvSdZcMvbvXda3dna"
+host = inputs["Host"]
+port = inputs["Port"]
+database = inputs["Database"]
+user = inputs["User"]
+password = inputs["Password"]
 
 
 # Connect to the database
@@ -51,8 +60,12 @@ cursor.execute(query)
 # Fetch all records
 table_list = cursor.fetchall()
 
+csv_directory = "csv_files"
+if not os.path.exists(csv_directory):
+    os.makedirs(csv_directory)
 
 for table_items in table_list:
+
 
     string_item = str(table_items)
 
@@ -65,8 +78,8 @@ for table_items in table_list:
     cursor.execute(query3)
     table = cursor.fetchall()
 
-    file_path = f"{string_item}.csv"
-   
+    file_path = os.path.join(csv_directory, f"{string_item}.csv")
+
     with open(file_path, mode='w', newline='') as file:
         writer = csv.writer(file)
         writer.writerows(table)
