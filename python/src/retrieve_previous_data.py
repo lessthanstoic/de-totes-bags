2083f1ae18cee548286abd59c3e505615d9dd11e
grandrei <florin.andreigr@gmail.com>
2023-07-26 11:34:13 +0100

    finished the function retrieve previous data

--- python/src/retrieve_previous_data.py
+++ python/src/retrieve_previous_data.py
@@ -4,11 +4,21 @@ import pandas as pd
 from botocore.exceptions import ClientError
 
 """
-
+This module reads .csv files from our ingestion bucket, and converts them to a pandas dataframe.
+This module contains one function:
+retrieve_previous_data
 """
 def retrieve_previous_data(table_name):
     """
-    
+    This function reads a .csv file from our ingestion bucket.
+    Arguments:
+    table_name (string) - represents the name of a table in our database.
+    Output:
+    resulting_df (DataFrame) - outputs the read .csv file as a pandas DataFrame for use with other functions
+    Errors:
+    TypeError - if input is not a string
+    ValueError - if input is not a valid table name
+
     """
 
     try:
@@ -21,17 +31,19 @@ def retrieve_previous_data(table_name):
         #connect to the s3
         s3 = boto3.client('s3')
 
+        #get the object with the input name
         file = s3.get_object(Bucket='ingested-data-vox-indicium', Key=file_name)
 
+        #convert the body of the object in a DateFrame
         resulting_df = pd.read_csv(file['Body'])
 
         return resulting_df
     
     except ClientError as e:
-
+        #catches the error if the user tap a non-existent table name
         if e.response['Error']['Code'] == 'NoSuchKey':
             raise ValueError(f"The file {table_name} does not exist")
         
     except TypeError as e:
-        
+        #catches the error if the user tap an incorrect input 
         raise TypeError("Function must take a string input")
