14493e92d3d78c84c79d73bac238a6f61aa58f04
lessthanstoic <activehelix@gmail.com>
2023-07-27 11:18:43 +0100

    Update csv test

--- python/tests/test_writecsv.py
+++ python/tests/test_writecsv.py
@@ -4,10 +4,10 @@ import pytest
 
 
 def test_writes_a_table_csv():
-    table_query = ["user_id","name","password",
-            "1","Andrei","Password123",
-            "2","Michael","Liverpool654"
-            "3","Mark","Edward34",
-            "4","Simon","!!QWasd"]
-    write_table_to_csv( table_query, "sales")
+    table_query = [["user_id", "name", "password"],
+            ["1", "Andrei", "Password123"],
+            ["2", "Michael", "Liverpool654"],
+            ["3", "Mark", "Edward34"],
+            ["4", "Simon", "!!QWasd"]]
+    write_table_to_csv(table_query, "sales")
     assert os.path.isfile("csv_files/sales.csv")
