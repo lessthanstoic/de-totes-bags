5f05b63d656e149f74cf8b54ad559c4009073d9f
lessthanstoic <activehelix@gmail.com>
2023-07-27 14:04:16 +0100

    Ensures that flake8 misses out the __init__.py file

--- python/src/__init__.py
+++ python/src/__init__.py
@@ -1,4 +1,4 @@
+# flake8: noqa
 from .csv_write import write_table_to_csv
-# from .postgres_data_capture import postgres_data_capture
 from .s3_timestamp import get_s3_timestamp
 from .secret_login import retrieve_secret_details
