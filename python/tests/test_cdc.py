6ab9825525572e96aa517635dc7cbfc842e86ddd
mickfay <mick.fay@hotmail.com>
2023-07-28 15:29:14 +0100

    Changed testing of secret_login functions to ensure maximum coverage, without use of os library that was causing errors

--- python/tests/test_cdc.py
+++ python/tests/test_cdc.py
@@ -1,25 +1,27 @@
-# from src.secret_login import retrieve_secret_details
+from python.src.secret_login import retrieve_secret_details
 import boto3
-# from moto import mock_secretsmanager
-import os
+from moto import mock_secretsmanager
+# import os
 import pytest
-# from botocore.exceptions import ClientError
+from botocore.exceptions import ClientError
+# from pprint import pprint
+import json
 
 
-@pytest.fixture(scope="function")
-def aws_credentials():
-    """Mocked AWS Credentials for moto."""
-    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
-    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
-    os.environ["AWS_SECURITY_TOKEN"] = "testing"
-    os.environ["AWS_SESSION_TOKEN"] = "testing"
-    os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
+# @pytest.fixture(scope="function")
+# def aws_credentials():
+#     """Mocked AWS Credentials for moto."""
+#     os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+#     os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+#     os.environ["AWS_SECURITY_TOKEN"] = "testing"
+#     os.environ["AWS_SESSION_TOKEN"] = "testing"
+#     os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
 
 
-@pytest.fixture(scope="session")
-def client():
-    conn = boto3.client("secretsmanager", region_name="eu-west-2")
-    yield conn
+# @pytest.fixture(scope="session")
+# def client():
+#     conn = boto3.client("secretsmanager", region_name="eu-west-2")
+#     yield conn
 
 
 # @mock_secretsmanager
@@ -45,3 +47,30 @@ def client():
 #     # ACT / ASSERT
 #     with pytest.raises(ClientError):
 #         retrieve_secret_details("on")
+
+@mock_secretsmanager
+def import_secret_to_mock(secret_id):
+    mock_client = boto3.client('secretsmanager')
+    mock_client.create_secret(Name=secret_id, SecretString=json.dumps(
+        {"username": 'test_user', "password": 'test_password'}))
+
+
+@mock_secretsmanager
+def test_retrieves_correct_secret():
+
+    import_secret_to_mock('test_secret')
+
+    answer = retrieve_secret_details('test_secret')
+
+    assert answer == {'password': 'test_password', 'username': 'test_user'}
+
+
+@mock_secretsmanager
+def test_raises_client_error():
+
+    import_secret_to_mock('test_secret')
+
+    with pytest.raises(ClientError):
+        retrieve_secret_details('no_secret')
+
+    pass
