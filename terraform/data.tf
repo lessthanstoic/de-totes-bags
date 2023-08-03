3b9cb723a842094bcb0cbfcea4d604ca56ff5e55
Amy <a.yang97@hotmail.com>
2023-08-03 14:43:41 +0100

    updated the file directory in data.tf

--- terraform/data.tf
+++ terraform/data.tf
@@ -5,24 +5,23 @@ data "aws_region" "current" {
   name = "eu-west-2"
 }
 
-# file paths need updating for the lambdas when in sub-folders
 data "archive_file" "ingestion_lambda" {
   type        = "zip"
-  source_dir = "${path.module}/../python"
-  output_path = "${path.module}/../function.zip"
+  source_dir = "${path.module}/../python/ingestion_function/src" 
+  output_path = "${path.module}/../ingestion_function.zip"
 }
 
 
 data "archive_file" "transform_lambda" {
   type        = "zip"
-  source_dir = "${path.module}/../python"
-  output_path = "${path.module}/../function.zip"
+  source_dir = "${path.module}/../python/transformation_function/src"
+  output_path = "${path.module}/../transformation_function.zip"
 }
 
 
 data "archive_file" "warehouse_lambda" {
   type        = "zip"
-  source_dir = "${path.module}/../python"
-  output_path = "${path.module}/../function.zip"
+  source_dir = "${path.module}/../python/loading_function/src"
+  output_path = "${path.module}/../loading_function.zip"
 }
 
