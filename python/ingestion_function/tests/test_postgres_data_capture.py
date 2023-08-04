cf17f18a58229f4e27ffea094cd625c1a00c11c5
lessthanstoic <activehelix@gmail.com>
2023-08-04 13:52:24 +0100

    Deleted all but the loading function from the python folder

--- python/ingestion_function/tests/test_postgres_data_capture.py
+++
@@ -1,6 +0,0 @@
-# from python.src.postgres_data_capture import postgres_data_capture
-# def test_function_pushes_information_to_bucket():
-
-#     postgres_data_capture('egg', 'egg')
-
-#     assert False
